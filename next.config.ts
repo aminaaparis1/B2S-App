f178974d2e732410d8783570e9d0808bf0e0137c
aminaaparis1 <amina.zouane@etu.univ-paris1.fr>
2026-05-19 16:32:23 +0200

    changement config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
+import type { NextConfig } from "next";
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   /* config options here */
 };
 
-module.exports = withPWA(nextConfig);
+export default nextConfig;
